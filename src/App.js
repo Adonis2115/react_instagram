0ee1bf7369990c1cb884ea0b1fdbc35c49089b30
Himanshu Pandey <himanshu21pandey@gmail.com>
2022-01-04 14:12:28 +0530

    Resolved Route Error

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 // import Layout from "./components/layout/Layout";
-import { Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
  import NewPost from './pages/NewPost';
 //  import Profile from './pages/Profile';
 //  import Setting from './pages/Setting';
 
 function App() {
   return (
-    <div>
-      <Route path='/'>
-        <NewPost />
-      </Route>
-      {/* <Route path='/path'>
-        <Profile />
-      </Route>
-      <Route path='/setting'>
-        <Setting />
-      </Route> */}
-    </div>
+    <Routes>
+      <Route path="/" element={<NewPost />} />
+    </Routes>
   );
 }
 
