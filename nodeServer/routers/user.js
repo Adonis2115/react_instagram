eb11b3686440869b09dc9cfeaa876c05f3bef86e
avi-lucky <shuklaavikal7@gmail.com>
2022-01-28 00:14:05 +0530

    forgot password

--- nodeServer/routers/user.js
+++ nodeServer/routers/user.js
@@ -72,24 +72,24 @@ router.post("/users/logout", auth, async (req, res) => {
   }
 });
 
-// // Forgot Password
-// router.patch("/users/forgot", async (req, res) => {
-//   const updates = Object.keys(req.body);
-//   // console.log(req.body)
-//   const allowedUpdates = ["password"];
-//   try {
-//     await User.updateOne(
-//       { email: req.body.email },
-//       { password: req.body.password }
-//     );
-//     const user = await User.findOne({ email: req.body.email });
-//     res.status(200).send(user);
-//     if (!user)
-//       return res.status(400).send("User With Given Email Doesn't Exist!");
-//   } catch (e) {
-//     console.log(e);
-//     res.status(500).send(e);
-//   }
-// });
+// Forgot Password
+router.patch("/users/forgot", async (req, res) => {
+  const updates = Object.keys(req.body);
+  // console.log(req.body)
+  const allowedUpdates = ["password"];
+  try {
+    await User.updateOne(
+      { email: req.body.email },
+      { password: req.body.password }
+    );
+    const user = await User.findOne({ email: req.body.email });
+    res.status(200).send(user);
+    if (!user)
+      return res.status(400).send("User With Given Email Doesn't Exist!");
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
+  }
+});
 
 module.exports = router;
