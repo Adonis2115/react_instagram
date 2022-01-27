51b52524f778e0b10cf37178b30e4a79a2f6e965
avi-lucky <shuklaavikal7@gmail.com>
2022-01-27 14:08:08 +0530

    user can update his/her profile

--- nodeServer/routers/user.js
+++ nodeServer/routers/user.js
@@ -37,26 +37,26 @@ router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
 });
 
-// // Update User Profile
-// router.patch("/users/me", auth, async (req, res) => {
-//   const updates = Object.keys(req.body);
-//   const allowedUpdates = ["name", "password"];
-//   const isValidOperation = updates.every((update) =>
-//     allowedUpdates.includes(update)
-//   );
+// Update User Profile
+router.patch("/users/me", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "password"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
 
-//   if (!isValidOperation) {
-//     return res.status(400).send({ error: "Invalid updates!" });
-//   }
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
 
-//   try {
-//     updates.forEach((update) => (req.user[update] = req.body[update]));
-//     await req.user.save();
-//     res.send(req.user);
-//   } catch (e) {
-//     res.status(400).send(e);
-//   }
-// });
+  try {
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
+    res.send(req.user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
 // // LogOut User
 // router.post("/users/logout", auth, async (req, res) => {
