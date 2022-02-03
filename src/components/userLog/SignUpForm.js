7c8330acf56f8815f246b9b3440cec441ddec181
avi-lucky <shuklaavikal7@gmail.com>
2022-02-03 18:24:16 +0530

    signup page ready

--- src/components/userLog/SignUpForm.js
+++ src/components/userLog/SignUpForm.js
@@ -1,3 +1,5 @@
+import classes from "./SignUpForm.module.css";
+
 import { useRef } from "react";
 
 function SignUp(props) {
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,5 +1,12 @@
+import SignUpForm from "../components/userLog/SignUpForm";
+
 function SignUp() {
-  return <div>Sign-Up</div>;
+  return (
+    <section>
+      <h1>Sign Up</h1>
+      <SignUpForm />
+    </section>
+  );
 }
 
 export default SignUp;
